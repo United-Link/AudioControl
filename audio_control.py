b968a17ec7e2c033125609113442e3fe3691db0a
hungshinlee <hungshinlee@gmail.com>
2024-11-14 17:04:12 +0800

    Refactor audio device checks and volume level queries for improved clarity and functionality

--- audio_control.py
+++ audio_control.py
@@ -22,8 +22,12 @@ def check_device_exists():
         return False
 
 
-def check_default_audio_devices():
+def query_default_audio_devices():
     """檢查預設 source 與 sink 是否都包含 "TASCAM_US-2x2HR" 字串"""
+
+    default_sink = None
+    default_source = None
+
     try:
         result = subprocess.run(
             ["pactl", "info"], capture_output=True, text=True, check=True
@@ -40,28 +44,28 @@ def check_default_audio_devices():
             else None
         )
 
-        if not default_sink or not default_source:
-            return False  # 找不到預設sink或source
-
-        return "TASCAM_US-2x2HR" in default_sink and "TASCAM_US-2x2HR" in default_source
+        return default_source, default_sink
     except (
         subprocess.CalledProcessError,
         AttributeError,
     ):  # 處理 pactl 錯誤或 regex 找不到的情況
-        return False
+        return default_source, default_sink
 
 
-def check_volume_levels():
+def query_volume_levels(source, sink):
     """檢查預設裝置的音量是否都是 100%"""
+    source_level = None
+    sink_level = None
+
     try:
         sink_volume = subprocess.run(
-            ["pactl", "get-sink-volume", "@DEFAULT_SINK@"],
+            ["pactl", "get-sink-volume", sink],
             capture_output=True,
             text=True,
             check=True,
         ).stdout
         source_volume = subprocess.run(
-            ["pactl", "get-source-volume", "@DEFAULT_SOURCE@"],
+            ["pactl", "get-source-volume", source],
             capture_output=True,
             text=True,
             check=True,
@@ -78,16 +82,13 @@ def check_volume_levels():
             else None
         )
 
-        if not sink_level or not source_level:
-            return False
-
-        return sink_level == 100 and source_level == 100
+        return source_level, sink_level
     except (
         subprocess.CalledProcessError,
         AttributeError,
         ValueError,
     ):  # 處理錯誤或找不到數值
-        return False
+        return source_level, sink_level
 
 
 def check_containers():
@@ -313,5 +314,9 @@ if __name__ == "__main__":
     # server.serve_forever()
     #
     print(f"check_device_exists: {check_device_exists()}")
-    print(f"check_default_audio_devices: {check_default_audio_devices()}")
-    print(f"check_volume_levels: {check_volume_levels()}")
+
+    default_source, default_sink = query_default_audio_devices()
+    if default_source is not None and default_sink is not None:
+        print(f"Default source: {default_source}, Default sink: {default_sink}")
+        source_level, sink_level = query_volume_levels(default_source, default_sink)
+        print(f"Source level: {source_level}, Sink level: {sink_level}")
